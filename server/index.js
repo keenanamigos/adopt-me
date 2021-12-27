0cee6ff3aad9624eb267e2d69e64cf90034bde98
Keenan Johnson <keenan.johnson1@outlook.com>
2021-12-27 10:38:29 -0500

    SSR strategy is working as expected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
-import { express } from "express";
+import express from "express";
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from "react-router-dom";
 import fs from 'fs';
 import AdoptionApp from '../src/app';
 
+const PORT = process.env.PORT || 3000;
+const html = fs.readFileSync("dist/index.html").toString();
+const parts = html.split("not rendered");
+
+const app = express();
+
+app.use("/dist", express.static("dist"));
+app.use((request, response) => {
+    const staticContext = {};
+    const reactMarkup = (
+        <StaticRouter url={request.url} context={staticContext}>
+            <AdoptionApp />
+        </StaticRouter>
+    )
+
+    response.status(staticContext.statusCode || 200);
+    response.send(`${parts[0]}${renderToString(reactMarkup)}${parts[1]}`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
