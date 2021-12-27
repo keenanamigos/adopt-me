0915282600535861c21e154075806489fb04d549
Keenan Johnson <keenan.johnson1@outlook.com>
2021-12-27 10:16:10 -0500

    Download express and setup imports for index.js file

---
+++ server/index.js
@@ -0,0 +1,6 @@
+import { express } from "express";
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from "react-router-dom";
+import fs from 'fs';
+import AdoptionApp from '../src/app';
+
