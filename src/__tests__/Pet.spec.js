59a51f6a030992c8a2374c946b2cb7f5b3384b9e
Keenan Johnson <keenan.johnson1@outlook.com>
2021-12-28 15:26:56 -0500

    Unit tests all passing

--- src/__tests__/Pet.spec.js
+++ src/__tests__/Pet.spec.js
@@ -14,4 +14,15 @@ describe("Pet Component UI", () => {
 
         expect(thumbnailImage.src).toContain("none.jpg");
     });
+
+    test("displays a non-default thumbnail image when one is given", async () => {
+        const pet = render(
+            <StaticRouter>
+                <Pet images={["1.jpg", "2.jpg", "3.jpg"]} />
+            </StaticRouter>
+        );
+        const thumbnailImage = await pet.findByTestId("thumbnail");
+        // It grabs the first item in the array of images
+        expect(thumbnailImage.src).toContain("1.jpg");
+    });
 });
