2bc9320cdda85b967045bf0cd1964fbbab38600f
Keenan Johnson <keenan.johnson1@outlook.com>
2021-12-28 14:40:07 -0500

    Fix typo to call devtools extension method

--- src/store.js
+++ src/store.js
@@ -3,10 +3,10 @@ import reducer from "./reducer/index";
 
 const store = createStore(
     reducer,
-    typeof window === "object" && 
-    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined" ? 
-    window.__REDUX_DEVTOOLS_EXTENSION__ : 
-    (f) => f
+    typeof window === "object" &&
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (f) => f
 );
 
 export default store;
