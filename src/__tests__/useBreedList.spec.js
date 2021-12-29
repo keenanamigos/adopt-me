e3a1a03bdbe660900a2a4d7023ad893263d1c99f
Keenan Johnson <keenan.johnson1@outlook.com>
2021-12-28 20:11:43 -0500

    Write useBreedList unit test, download Fetch mock, update package.json

--- src/__tests__/useBreedList.spec.js
+++ src/__tests__/useBreedList.spec.js
@@ -1,23 +1,11 @@
 import { describe, expect, test } from "@jest/globals";
-import { render } from "@testing-library/react";
+import { renderHook } from "@testing-library/react-hooks";
 import useBreedList from "../useBreedList";
-
-function getBreedList(animal) {
-    let list;
-
-    function TestComponent() {
-        list = useBreedList(animal);
-        return null;
-    }
-
-    render(<TestComponent />);
-
-    return list;
-}
 
 describe("useBreedList", () => {
     test("gives an empty array with no animal", async () => {
-        const [breedList, status] = getBreedList();
+        const { result } = renderHook(() => useBreedList());
+        const [breedList, status] = result.current;
 
         expect(breedList.length).toEqual(0);
         expect(status).toEqual("unloaded");
